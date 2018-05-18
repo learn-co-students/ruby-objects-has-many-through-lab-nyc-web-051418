4605a0e1c6069ff6f7ece1209b65e69a7665d597
Alberto Carreras <a.carreras.c@gmail.com>
2018-05-18 17:34:56 -0400

    Solved lab

---
+++ lib/appointment.rb
@@ -0,0 +1,18 @@
+require 'pry'
+
+class Appointment
+  attr_accessor :date, :doctor, :patient
+
+  @@all = []
+
+  def initialize(doctor, patient, date)
+    @date = date
+    @doctor = doctor
+    @patient = patient
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+end
