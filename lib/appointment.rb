a343a8a6f69eff311cb6143ec05e9da2de879e7f
Ledio <L.Bode515@gmail.com>
2018-05-19 23:51:56 -0400

    Done.

---
+++ lib/appointment.rb
@@ -0,0 +1,21 @@
+require 'pry'
+
+class Appointment
+
+  attr_accessor :date, :patient, :doctor
+
+  ALL = []
+
+  def initialize(patient, doctor, date)
+    @patient = patient
+    @doctor = doctor
+    @date = date
+
+
+  ALL << self
+  end
+
+  def self.all
+    ALL
+  end
+end
---
+++ lib/doctor.rb
@@ -0,0 +1,33 @@
+require 'pry'
+require_relative 'appointment.rb'
+
+class Doctor
+
+  attr_accessor :name
+  ALL = []
+
+  def initialize(name)
+  @name = name
+  ALL << self
+  end
+
+  def self.all
+    ALL
+  end
+
+  def new_appointment(patient, date)
+    Appointment.new(patient, self, date)
+  end
+
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.doctor == self
+    end
+  end
+
+  def patients
+    appointments.map do |appointment|
+      appointment.patient
+    end
+  end
+end
